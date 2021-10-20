d13cab7ff3c6ee8c37381f4364240e14a0a01593
Erez <erezfree29@gmail.com>
2021-10-20 11:14:13 +0100

    add greeting controller actions

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,2 +1,11 @@
 class Api::V1::GreetingsController < ApplicationController
+    def index
+      @greetings = Greeting.all
+      render json:@greetings 
+    end
+
+    def show
+      @greeting
+      render json:@greeting    
+    end
 end
