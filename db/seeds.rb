3ae452c115a0e0d7f25978bf234c7450e8bac8a6
Erez <erezfree29@gmail.com>
2021-10-20 10:53:14 +0100

    add messages table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Seeding'
+
+Greeting.create(message:'Hola amigo')
+Greeting.create(message:'Nice to see you again')
+Greeting.create(message:'Welcome my friend')
+Greeting.create(message:'Hello and welcome')
+Greeting.create(message:'Shalom haver')
