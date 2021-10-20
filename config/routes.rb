91fa7717e5a3ff875c4282d4571f85907bcc9eb8
Erez <erezfree29@gmail.com>
2021-10-20 11:21:19 +0100

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    root 'pages#indes'
+    namespace :api do
+      namespace :v1 do
+        resources :greetings ,only: [:index, :show]
+      end
+    end
 end
